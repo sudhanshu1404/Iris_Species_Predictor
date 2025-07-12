2471072019cf8862ce574fdad32a3494233f9966
Sudhanshu Chaudhary <112551947+sudhanshu1404@users.noreply.github.com>
2025-07-12 10:13:07 +0530

    Add files via upload

---
+++ save_model.py
@@ -0,0 +1,13 @@
+# save_model.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load data and train
+iris = load_iris()
+X, y = iris.data, iris.target
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, 'iris_model.joblib')
